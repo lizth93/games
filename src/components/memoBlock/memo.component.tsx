517173a088ebd60b76af287cc950bb98afedebe8
lizth93 <lizth93@hotmail.com>
2023-02-19 11:22:27 -0500

    The images are rendered

--- src/components/memoBlock/memo.component.tsx
+++ src/components/memoBlock/memo.component.tsx
@@ -1,27 +1,34 @@
+import React from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "store";
 import { getImages } from "store/getImages";
-import { RootState } from "store/imagesSlice";
 
-function MemoBlock() {
+function MemoBlock(): JSX.Element {
   const dispatch: any = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(getImages());
   }, [dispatch]);
 
-  const { images, isLoadingImages } = useSelector((state: RootState) => ({
+  const { images, isLoadingImages } = useSelector((state: any) => ({
     images: state.imagesCollection.images,
     isLoadingImages: state.imagesCollection.isLoadingImages,
   }));
 
-  console.log(images);
+  if (isLoadingImages) {
+    return <p>Please Wait</p>;
+  }
+
   return (
     <>
       {images.map((image: any) => (
-        <p key={image.id}>{image.featured_gif.bitly_url}</p>
+        <img
+          src={image.featured_gif.images.fixed_height_small.url}
+          alt={image.display_name}
+          key={image.id}
+        />
       ))}
     </>
   );
