2a266217eed5cd03aba8dd9a447d487c617ac843
lizth93 <lizth93@hotmail.com>
2023-02-19 13:06:54 -0500

    Styles are added and the images are arranged randomly

--- src/components/memoBlock/memo.component.tsx
+++ src/components/memoBlock/memo.component.tsx
@@ -5,7 +5,11 @@ import { useDispatch } from "react-redux";
 import { AppDispatch } from "store";
 import { getImages } from "store/getImages";
 
-function MemoBlock(): JSX.Element {
+interface Props {
+  className?: string;
+}
+
+function MemoBlock(props: Props): JSX.Element {
   const dispatch: any = useDispatch<AppDispatch>();
 
   useEffect(() => {
@@ -20,17 +24,21 @@ function MemoBlock(): JSX.Element {
   if (isLoadingImages) {
     return <p>Please Wait</p>;
   }
+  console.log(images);
+
+  const shuffledImages = images.slice().sort(() => 0.5 - Math.random());
 
   return (
-    <>
-      {images.map((image: any) => (
+    <div className={props.className}>
+      {shuffledImages.map((image: any) => (
         <img
+          className="img-block"
           src={image.featured_gif.images.fixed_height_small.url}
           alt={image.display_name}
           key={image.id}
         />
       ))}
-    </>
+    </div>
   );
 }
 export default MemoBlock;
