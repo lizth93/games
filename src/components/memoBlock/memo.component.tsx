427fa61165cd79549afa72b6b4cfd8d4d0f470cc
lizth93 <lizth93@hotmail.com>
2023-02-26 13:37:30 -0500

    Deleted variable without use

--- src/components/memoBlock/memo.component.tsx
+++ src/components/memoBlock/memo.component.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, memo } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch } from "store";
 import { getImages } from "store/getImages";
