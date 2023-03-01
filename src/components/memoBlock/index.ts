38e22c9860625ff62f5b1e6f01847c74d019ab41
lizth93 <lizth93@hotmail.com>
2023-02-28 21:11:21 -0500

    Commentaries about unit em are fixed

--- src/components/memoBlock/index.ts
+++ src/components/memoBlock/index.ts
@@ -10,32 +10,32 @@ export default styled(MemoBlock)`
   gap: 2px;
   margin-top: 2rem;
 
+  // 55.6em = 890px
   @media screen and (max-width: 55.6em) {
-    // 55.6em = 890px
     margin-top: 1rem;
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: repeat(8, 1fr);
   }
+
   .img-block {
     box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
       rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
       rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-
     width: 100px;
     height: 100px;
-
     position: relative;
     display: inline-block;
     cursor: pointer;
     aspect-ratio: 1 / 1;
 
+    //25em = 400px
     @media screen and (max-width: 25em) {
-      //25em = 400px
       gap: 1px;
       width: 80px;
       height: 80px;
     }
 
+    //20.6em = 329px
     @media screen and (max-width: 20.6em) {
       gap: 1px;
       width: 70px;
