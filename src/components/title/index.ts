0ec7bca56a39fc379b5e0e0a138927264a5c65bb
lizth93 <lizth93@hotmail.com>
2023-03-16 18:54:19 -0500

    Fixed title styles and added dark background

--- src/components/title/index.ts
+++ src/components/title/index.ts
@@ -13,29 +13,29 @@ export default styled(Title)`
     animation: stroke 6s infinite linear;
 
     &:nth-child(5n + 1) {
-      stroke: #f2385a;
+      stroke: #5c940d;
       -webkit-animation-delay: -1.2s;
-      animation-delay: -2.2s;
+      animation-delay: -1.2s;
     }
     &:nth-child(5n + 2) {
       stroke: #f5a503;
       -webkit-animation-delay: -2.4s;
-      animation-delay: -4.4s;
+      animation-delay: -2.4s;
     }
     &:nth-child(5n + 3) {
       stroke: #e9f1df;
       -webkit-animation-delay: -3.6s;
-      animation-delay: -5.6s;
+      animation-delay: -3.6s;
     }
     &:nth-child(5n + 4) {
       stroke: #56d9cd;
       -webkit-animation-delay: -4.8s;
-      animation-delay: -6.8s;
+      animation-delay: -4.8s;
     }
     &:nth-child(5n + 5) {
       stroke: #3aa1bf;
       -webkit-animation-delay: -6s;
-      animation-delay: -8s;
+      animation-delay: -6s;
     }
   }
 
